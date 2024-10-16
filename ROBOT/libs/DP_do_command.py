6de1565d7272c237d500a21c9cfea7e42f288c79
PanteleevDanil <163186780+PanteleevDanil@users.noreply.github.com>
2024-10-16 14:23:47 +0500

    f

--- ROBOT/libs/DP_do_command.py
+++ ROBOT/libs/DP_do_command.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Получаем путь к директории ROBOT
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# Добавляем libs в sys.path
+sys.path.append(parent_dir)
+
 import libs.DP_MotorMoveLibr as Motor
 def do_command_RASP(name, values):
     pass
