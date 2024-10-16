579f9eb7a3fc36121b88c396b3c70ea9c8283901
Dakons <76495215+Dakons@users.noreply.github.com>
2024-10-16 17:34:04 +0500

    Новый формат нескольких процессов с мониторингом

--- ROBOT/libs/DP_tasks.py
+++ ROBOT/libs/DP_tasks.py
@@ -1,10 +1,9 @@
 import sys
 import os
 import time
-
+import threading
 # Константы
 BAZASPEED = 5
-
 
 # Получаем путь к директории ROBOT
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -19,14 +18,16 @@ from libs.DP_teleplot import TelemetrySender
 from libs.DP_Filter import Filter
 from libs.DP_servo import Servo
 import libs.DP_MotorMovements as Movement
+
 # Фильтр для сенсора
 Filter_sonar = Filter(5, 0.3)
-
+distance = Ultrasonic.get_distance()
+distance_filtered = round(Filter_sonar.filter(distance))
 # Телеметрия
 MoveData = TelemetrySender()
 
 # Моторное движение и серво
-sonServo = Servo(ANGLE_MAX = 180, ANGLE_MIN = 0, servonum=7)
+sonServo = Servo(ANGLE_MAX=180, ANGLE_MIN=0, servonum=7)
 sonServo.set(90)
 
 def constrain(value, min_value, max_value):
@@ -36,7 +37,7 @@ def constrain(value, min_value, max_value):
 def drive_along_wall(side: str, distance, setpoint, Kp, Ki, Kd):
     """
     Управляет роботом для движения вдоль стены заданное время.
-    
+
     Параметры:
         side (str): 'left' или 'right', указывает, с какой стороны находится стена.
         distance (float): Расстояние
@@ -48,62 +49,55 @@ def drive_along_wall(side: str, distance, setpoint, Kp, Ki, Kd):
     drive_time = distance * 1
 
     # Инициализация регулятора
-    VectorRegulator = PIDRegulator(Kp=Kp, Ki=Ki, Kd=Kd,output_min=-2,output_max=2)
+    VectorRegulator = PIDRegulator(Kp=Kp, Ki=Ki, Kd=Kd, output_min=-2, output_max=2)
 
     # Засекаем время начала
     start_time = time.time()
     sweep_permission = "YES"
     sweep_timemarker = 0.0
-    while True:
+    
+    # Запуск потока для мониторинга данных
+    telemetry_thread = threading.Thread(target=Telemetry)
+    telemetry_thread.start()
 
+    while True:  # Воздействия
         # Логика скорости с учетом положения стены
         if side == 'LEFT':
             sonServo.set(180)
-            # Если стена слева
-            #Left_Speed = BAZASPEED - Vector
-            #Right_Speed = BAZASPEED + Vector
         elif side == 'RIGHT':
             sonServo.set(0)
-            # Если стена справа
-           # Left_Speed = BAZASPEED + Vector
-            #Right_Speed = BAZASPEED - Vector
         else:
             raise ValueError("Неверное значение для 'side'. Ожидается 'left' или 'right'.")
-        # Получаем расстояние от датчика
-        distance = Ultrasonic.get_distance()
-        distance_filtered = round(Filter_sonar.filter(distance))
-        #MoveData.send_telemetry("Distance", distance_filtered)
-        print(distance_filtered)
-        
-        
+
         # ПИД регуляция расстояния до стены
         Vector = VectorRegulator.regulate(distance_filtered, setpoint)
         Vector = Vector * 0.1
-        
-        if (time.time() - sweep_timemarker) > Vector:
-            sweep_permission == "YES"
-        # Передаем скорости на моторы
-        
-        if sweep_permission == "YES" and Vector !=0:
-            sweep_permission == "NO"
-            sweep_timemarker = time.time()
-            Movement.sweep(BAZASPEED, 100, 0.1, side)#Нужно намутить контроль по времени, чтобы не делалось больше чем надо
-            
-        Motor.MotorMove(BAZASPEED, BAZASPEED)  
 
-        # Отправка телеметрии
-        
-        #MoveData.send_telemetry("P", VectorRegulator.P)
-        #MoveData.send_telemetry("I", VectorRegulator.I)
-        #MoveData.send_telemetry("D", VectorRegulator.D)
+        if (time.time() - sweep_timemarker) > Vector:
+            sweep_permission = "YES"
+
+        # Передаем скорости на моторы
+        if sweep_permission == "YES" and Vector != 0:
+            sweep_permission = "NO"
+            sweep_timemarker = time.time()
+            Movement.sweep(BAZASPEED, 100, 0.1, side)
+
+        Motor.MotorMove(BAZASPEED, BAZASPEED)
+
         MoveData.send_telemetry("Error", VectorRegulator.regulate_error)
         MoveData.send_telemetry("Vector", Vector)
-        
+
         # Проверяем время, чтобы завершить выполнение через заданное время
         current_time = time.time()
         if (current_time - start_time) > drive_time:
             break
+    
+    # Ожидание завершения потока мониторинга
+    telemetry_thread.join()
 
-    # Остановка моторов после завершения движения
-    Motor.MotorMove(0, 0)
-
+def Telemetry():
+    """Мониторинг и отправка данных телеметрии."""
+    while True:  # Мониторинг
+        distance = Ultrasonic.get_distance()
+        distance_filtered = round(Filter_sonar.filter(distance))
+        MoveData.send_telemetry("Distance", distance_filtered)
--- ROBOT/src/MoveLong_test.py
+++ ROBOT/src/MoveLong_test.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import threading
+
 import time
 import libs.DP_tasks as Task
 import libs.DP_MotorMoveLibr as Motor
@@ -11,32 +11,16 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 # Добавляем libs в sys.path
 sys.path.append(parent_dir)
 
-# Флаг для управления завершением работы потока
-sweep_running = True
 
-def sweep_thread():
-    global sweep_running
-    while sweep_running:
-        # Выполняем движение sweep в отдельном потоке
-        Movement.sweep(5, 100, 0.1, "RIGHT")
-        time.sleep(0.1)  # Это для того, чтобы предотвратить чрезмерно быструю итерацию цикла
 
 # Основная программа
 Motor.MotorMove(0, 0)
 time.sleep(2)
 
-# Запускаем поток для sweep
-sweep_thread_instance = threading.Thread(target=sweep_thread)
-sweep_thread_instance.start()
-
 # Запускаем движение вдоль стены
 Task.drive_along_wall(side="RIGHT", distance=30, setpoint=100, Kp=1, Ki=0, Kd=0)
 
-# По окончании движения выключаем поток sweep
-sweep_running = False
 
-# Ждём завершения потока перед завершением программы
-sweep_thread_instance.join()
+Motor.MotorMove(0,0)
+Motor.MotorMove(0,0)
 
-# Остановка моторов
-Motor.MotorMove(0, 0)
