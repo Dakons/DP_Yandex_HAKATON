676e4d368afa0e84bfde4995b15a1ff250bfe1e6
Dakons <76495215+Dakons@users.noreply.github.com>
2024-10-18 14:59:31 +0500

    Update DP_tasks.py

--- ROBOT/libs/DP_tasks.py
+++ ROBOT/libs/DP_tasks.py
@@ -33,26 +33,30 @@ def drive_along_wall(side, Duration, setpoint, kp, ki, kd):
     
     current_pid_output = 0
     FirstTime = time.time()
+
     while True:
         Dist = Ultrasonic.get_distance()
         Dist_filtered = round(SonarFilter.filter(Dist))
 
         #print(f"Raw distance: {Dist}, Filtered distance: {Dist_filtered}")
-        if side == 'RIGHT':
+        if side == "RIGHT":
                 print("RIGHT")
                 Motor.MotorMove(BAZASPEED - current_pid_output, BAZASPEED + current_pid_output)
-        elif side == 'LEFT':
+        elif side == "LEFT":
                 print("LEFT")
                 Motor.MotorMove(BAZASPEED + current_pid_output, BAZASPEED - current_pid_output)
         DataTeleplot.send_telemetry("DISTANTION", Dist_filtered)
 
-        if time.time()-FirstTime>Duration:
+        if time.time() - FirstTime > Duration:
+             print("Task Completed. timer is off")
              break
         #time.sleep(0.01)
 
 
 
-drive_along_wall(side ="LEFT", Duration = 10, setpoint = 50, kp = 12, ki = -0.04, kd = 0)
+drive_along_wall(side ="LEFT", Duration = 5, setpoint = 50, kp = 12, ki = -0.04, kd = 0)
+Motor.MotorMove(0, 0)
 time.sleep(1)
-drive_along_wall("RIGHT", 10, 50, 12, -0.04, 0)
+#drive_along_wall("RIGHT", 10, 50, 12, -0.04, 0)
+#Motor.MotorMove(0, 0)
     
