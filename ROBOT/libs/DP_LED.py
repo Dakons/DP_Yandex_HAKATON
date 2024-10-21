f227e167014bbf5ae06589fcc30832265c9345aa
Dakons <76495215+Dakons@users.noreply.github.com>
2024-10-21 17:02:01 +0500

    Update DP_LED.py

--- ROBOT/libs/DP_LED.py
+++ ROBOT/libs/DP_LED.py
@@ -11,7 +11,7 @@ from libs.DP_i2c import I2c
 
 i2c = I2c()
 
-colors = {"GREEN": 2, "RED": 3}
+colors = {"GREEN": 2, "RED": 1}
 
 
 def set_all_leds(color):
@@ -22,6 +22,6 @@ def set_all_leds(color):
     :param color: цвет светодиодов (1-8, предопределённые значения)
     """
     # Устанавливаем цвет для всех 8 светодиодов в указанной группе
-    sendbuf = [0xff, 3, 8, colors[color], 0xff]
+    sendbuf = [0xff, 0x06, colors[color], 0x00, 0xff]
     i2c.writedata(i2c.mcu_address, sendbuf)
     time.sleep(0.01)
