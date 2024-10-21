a1bdb8f9376d690d87399aa83ef5d92566e2eb58
Dakons <76495215+Dakons@users.noreply.github.com>
2024-10-21 16:49:39 +0500

    Update DP_LED.py

--- ROBOT/libs/DP_LED.py
+++ ROBOT/libs/DP_LED.py
@@ -1,5 +1,13 @@
 import time
-from DP_i2c import I2c
+import sys
+import os
+
+# Получаем путь к директории ROBOT
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# Добавляем libs в sys.path
+sys.path.append(parent_dir)
+
+from libs.DP_i2c import I2c
 
 i2c = I2c()
 
@@ -16,11 +24,10 @@ class CarLight:
         :param group: группа светодиодов (1 для индикаторов заряда, 2 для фар)
         :param color: цвет светодиодов (1-8, предопределённые значения)
         """
-        if 0 < group < 3 and 0 < color < 9:
-            # Устанавливаем цвет для всех 8 светодиодов в указанной группе
-            sendbuf = [0xff, 3, 8, colors[color], 0xff]
-            i2c.writedata(i2c.mcu_address, sendbuf)
-            time.sleep(0.01)
+        # Устанавливаем цвет для всех 8 светодиодов в указанной группе
+        sendbuf = [0xff, 3, 8, colors[color], 0xff]
+        i2c.writedata(i2c.mcu_address, sendbuf)
+        time.sleep(0.01)
 
 # Пример использования:
 car_light = CarLight()
